eca02ff97852c380135c92658cc6c32504101dcc
ChenLin12138 <scorpion_chenlin@163.com>
2024-08-19 11:04:15 +0800

    0819

---
+++ src/leetcode/backtracking/combinations/LetterCasePermutation784.java
@@ -0,0 +1,18 @@
+package leetcode.backtracking.combinations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class LetterCasePermutation784 {
+  //字母可以变化大小，数字不行，这不就是一个组合问题吗
+  Stack<String> subResult = new Stack<>();
+  List<String> result = new ArrayList<>();
+  public List<String> letterCasePermutation(String s) {
+    return result;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
