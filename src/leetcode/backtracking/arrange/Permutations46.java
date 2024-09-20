84322e7e1acc4d2d7bff366ee4e3f1949d8cc97c
ChenLin12138 <scorpion_chenlin@163.com>
2024-09-20 16:24:34 +0800

    0920

--- src/leetcode/backtracking/arrange/Permutations46.java
+++ src/leetcode/backtracking/arrange/Permutations46.java
@@ -1,62 +1,52 @@
 package leetcode.backtracking.arrange;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @author Chen Lin
  * @date 2021-09-12
  */
 
 public class Permutations46 {
+  //此题求全排列，没有重复元素所以不用考虑剪枝
+  List<List<Integer>> res = new ArrayList<>();
+  Stack<Integer> stack = new Stack<>();
 
+  public List<List<Integer>> permute(int[] nums) {
+    backTracking(nums);
+    return res;
+  }
 
-	//输入[1,2],[a,b],[A,B]
-	//输出全排列[
-	//[1,a,A],[1,a,B],[1,b,A],[1,b,B],
-	//[2,a,A],[2,a,B],[2,b,A],[2,b,B]
-	//]
-	public char[][] solution(char[] array1, char[] array2, char[] array3) {
+  private void backTracking(int[] nums){
+    //退出条件
+    if(nums.length == 0){
+      //收集结果
+      res.add(new ArrayList<>(stack));
+      return;
+    }
 
-		//这里的3表示的传入多少个数组
-		int inputArgs = 3;
-		char [][] res = new char[array1.length * array2.length * array3.length][inputArgs];
-
-		int i = 0;
-		for(char e1 : array1) {
-			for(char e2 : array2) {
-				char[] temp = null;
-				for(char e3 : array3) {
-					temp = new char[] {e1,e2,e3};
-					res[i] = temp;
-					i ++;
-				}
-
-			}
-		}
-
-		return res;
-	}
+    //单层逻辑
+    for(int i = 0 ; i < nums.length; i ++){
+      //选取
+      stack.add(nums[i]);
+      //回溯
+      int[] newArray = new int[nums.length - 1];
+      System.arraycopy(nums,0,newArray,0,i);
+      System.arraycopy(nums,i+1,newArray,i,nums.length - i - 1);
+      backTracking(newArray);
+      //清理
+      stack.pop();
+    }
+  }
 
 	//此方法用于测试
 	public static void main(String[] args) {
-		Permutations46 ins = new Permutations46();
-		char[][] res = ins.solution(new char[] {'1','2'}, new char[] {'a','b'},new char[] {'A','B'});
-		System.out.print("[");
-		for(int i = 0; i < res.length; i ++) {
-			System.out.print("[");
-			for(int j = 0; j < 3; j ++) {
-				if(j == 2) {
-					System.out.print(res[i][j]);
-				}else {
-					System.out.print(res[i][j]+",");
-				}
-			}
-			if(i == res.length -1) {
-				System.out.print("]");
-			}else {
-				System.out.print("],");
-			}
-
-		}
-		System.out.print("]");
+    Permutations46 ins = new Permutations46();
+    List<List<Integer>> list = ins.permute(new int[] {1,2,3});
+    System.out.println(list);
 	}
 
 }
