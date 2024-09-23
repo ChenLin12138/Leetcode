f6fe30939e07609da5c51ce5e10c83d2a77068b7
May-zhou <279074071@qq.com>
2024-09-23 12:03:53 +0800

    20240923

--- src/leetcode/backtracking/arrange/Permutations46.java
+++ src/leetcode/backtracking/arrange/Permutations46.java
@@ -11,42 +11,34 @@ import java.util.Stack;
  */
 
 public class Permutations46 {
-  //此题求全排列，没有重复元素所以不用考虑剪枝
+
   List<List<Integer>> res = new ArrayList<>();
   Stack<Integer> stack = new Stack<>();
 
   public List<List<Integer>> permute(int[] nums) {
+
     backTracking(nums);
     return res;
   }
 
-  private void backTracking(int[] nums){
+  private void backTracking(int[] nums) {
     //退出条件
     if(nums.length == 0){
-      //收集结果
       res.add(new ArrayList<>(stack));
       return;
     }
 
     //单层逻辑
-    for(int i = 0 ; i < nums.length; i ++){
+    for(int i = 0; i < nums.length; i ++){
       //选取
       stack.add(nums[i]);
       //回溯
-      int[] newArray = new int[nums.length - 1];
-      System.arraycopy(nums,0,newArray,0,i);
-      System.arraycopy(nums,i+1,newArray,i,nums.length - i - 1);
-      backTracking(newArray);
+      int[] subSet = new int[nums.length - 1];
+      System.arraycopy(nums,0,subSet,0,i);
+      System.arraycopy(nums,i+1,subSet,i,nums.length - 1 - i);
+      backTracking(subSet);
       //清理
       stack.pop();
     }
   }
-
-	//此方法用于测试
-	public static void main(String[] args) {
-    Permutations46 ins = new Permutations46();
-    List<List<Integer>> list = ins.permute(new int[] {1,2,3});
-    System.out.println(list);
-	}
-
 }
